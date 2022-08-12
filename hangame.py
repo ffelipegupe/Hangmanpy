314ef148d410ea17bc30df03bc1059804f76c2ba
Pulzo 2.0 <pulzo2.0@MacBook-Pro-de-Pulzo.local>
2022-08-12 09:13:28 -0500

    Update: adding api to get random words

--- hangame.py
+++ hangame.py
@@ -1,4 +1,6 @@
 import random
+import requests
+
 
 #Function that capitalize the name entered if it is not
 def cap(name):
@@ -9,7 +11,8 @@ def cap(name):
         return name
 
 def worddic():
-    word = random.choice(["dog", "cat", "cow", "horse", "pokemon", "banana", "avocado", "tiger", "orange", "avengers"])
+    word = requests.get("https://random-word-api.herokuapp.com/word?lang=en").json()[0]
+    #word = random.choice(["dog", "cat", "cow", "horse", "pokemon", "banana", "avocado", "tiger", "orange", "avengers"])
     validLetters = 'abcdefghijklmnopqrstuvwxyzABCEDFGHIJKLMNSOPQRSTUVWXYZ'
     guessMade = ''
     count = 0
@@ -22,14 +25,16 @@ def worddic():
                 main = main + "_" + " "
 
         if main == word:
-            print ("The word was: ", main)
+            print("----------------------------------------")
+            print ("The word was: * " +  main + " *")
             print("Congratulations, you won the game!")
+            print("----------------------------------------")
             break
         print("Guess the word: ", main)
         print("Enter guess: ")
         guess = input()
 
-        if guess in validLetters:
+        if guess in validLetters and guess in word:
             converUpper = guess.lower()
             print("Good guess")
             guessMade = guessMade + converUpper
@@ -42,59 +47,81 @@ def worddic():
         if guess.lower() not in word:
             count = count + 1
             if count == 1:
+                print("----------------------------------------")
                 print("WRONG!")
                 print("   O   ")
+                print("----------------------------------------")
             if count == 2:
+                print("----------------------------------------")
                 print("WRONG!")
                 print("   O  ")
                 print("   |  ")
+                print("----------------------------------------")
             if count == 3:
+                print("----------------------------------------")
                 print("WRONG!")
                 print("   O  ")
                 print("   |  ")
                 print("  /   ")
+                print("----------------------------------------")
             if count == 4:
+                print("----------------------------------------")
                 print("WRONG!")
                 print("   O  ")
                 print("   |  ")
                 print("  / \ ")
+                print("----------------------------------------")
             if count == 5:
+                print("----------------------------------------")
                 print("WRONG!")
                 print(" \ O  ")
                 print("   |  ")
                 print("  / \ ")
+                print("----------------------------------------")
             if count == 6:
+                print("----------------------------------------")
                 print("WRONG!")
                 print(" \ O /")
                 print("   |  ")
                 print("  / \ ")
+                print("----------------------------------------")
             if count == 7:
+                print("----------------------------------------")
                 print("WRONG!")
                 print(" \ O|/")
                 print("   |  ")
                 print("  / \ ")
+                print("----------------------------------------")
             if count == 8:
+                print("----------------------------------------")
                 print("WRONG!")
                 print(" \ O_|/")
                 print("   |   ")
                 print("  / \  ")
+                print("----------------------------------------")
             if count == 8:
+                print("----------------------------------------")
                 print("WRONG! Last chance")
                 print(" \ O_||/")
                 print("   |   ")
                 print("  / \  ")
+                print("----------------------------------------")
             if count == 9:
+                print("----------------------------------------")
                 print("WRONG! A kind man just die!")
                 print("   O_||")
                 print(" / | \  ")
                 print("  / \  ")
                 print("The word wanted was:", word)
                 print("Better luck next time")
+                print("----------------------------------------")
                 break
 name = input("Enter your name: ")
+print("---")
 print("Welcome,", cap(name))
 print("Les't play Hangman!")
 print("----------------------------------------")
 print("Guess the word before the man is hanged.")
+print("----------------------------------------")
 print("----------------------------------------")
 worddic()
